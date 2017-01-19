b3641bd4f69f651f6774da120f8852e447124276
chatty-lytcone <chatty@lytcone.io>
2017-01-19 14:06:43 +0530

    one more dummy change

--- cityhash/src/city.cc
+++ cityhash/src/city.cc
@@ -634,6 +634,7 @@ uint128 CityHashCrc128WithSeed(const char *s, size_t len, uint128 seed) {
 }
 
 uint128 CityHashCrc128(const char *s, size_t len) {
+  int a = 123;
   if (len <= 900) {
     return CityHash128(s, len);
   } else {
